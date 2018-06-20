99f97f7e9216305742c61b1bc836fca7f0b588ee
RenatoNappo <31985727+RenatoNappo@users.noreply.github.com>
2018-06-20 14:12:51 +0100

    Add project files.

---
+++ 2FA-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2FA_2.MvcApplication" Language="C#" %>
---
+++ 2FA-2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2FA_2.Startup))]
+namespace _2FA_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
